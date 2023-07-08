b0b7e2a7c4148023334bc2c25dd231fe31a63987
lucascastellani <lucas100castellani@gmail.com>
2023-07-07 21:29:21 -0300

    feat: connection succeed

--- node/index.js
+++ node/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const mysql = require('mysql2')
 
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config)
 const app = express()
 const port = 3000
+
+const sql = `INSERT INTO nodedb.people VALUES(1, 'Castellani')`;
+connection.query(sql);
+connection.end();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
